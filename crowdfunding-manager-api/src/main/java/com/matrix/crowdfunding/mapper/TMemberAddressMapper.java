3d379844af1de8045df7858139242d308cdcac1c
AbelRose <15941999082@163.com>
2021-01-26 23:12:47 +0800

    20200126

---
+++ crowdfunding-manager-api/src/main/java/com/matrix/crowdfunding/mapper/TMemberAddressMapper.java
@@ -0,0 +1,30 @@
+package com.matrix.crowdfunding.mapper;
+
+import com.matrix.crowdfunding.bean.TMemberAddress;
+import com.matrix.crowdfunding.bean.TMemberAddressExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface TMemberAddressMapper {
+    long countByExample(TMemberAddressExample example);
+
+    int deleteByExample(TMemberAddressExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(TMemberAddress record);
+
+    int insertSelective(TMemberAddress record);
+
+    List<TMemberAddress> selectByExample(TMemberAddressExample example);
+
+    TMemberAddress selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") TMemberAddress record, @Param("example") TMemberAddressExample example);
+
+    int updateByExample(@Param("record") TMemberAddress record, @Param("example") TMemberAddressExample example);
+
+    int updateByPrimaryKeySelective(TMemberAddress record);
+
+    int updateByPrimaryKey(TMemberAddress record);
+}
